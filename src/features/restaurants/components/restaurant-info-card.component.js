b0b96bff58937a1f86e29b66bd51038061b885cc
pritamp17 <pritampawar625@gmail.com>
2022-06-18 13:12:51 +0530

    fake data

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,40 +1,61 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
-import { Card } from "react-native-paper";
-import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import {Text  } from "react-native";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
-const RestaurantCard = styled(Card)`
-  background-color: white;
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
-  background-color: white;
-`;
-
-const Title = styled(Text)`
-  padding: 16px;
-  color: red;
-`;
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
-    rating = 4, 
-    isClosedTemporarily,
+    rating = 4,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title >{name}</Title>
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
-
