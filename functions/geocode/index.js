49a0b2a791149c2682f2b0585a5c387e7453b5e8
pritamp17 <pritampawar625@gmail.com>
2022-06-28 12:58:45 +0530

    used this branch while using google cloud api

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,9 +1,28 @@
 const { locations: locationsMock } = require("./geocode.mock");
 const url = require("url");
+const functions = require("firebase-functions");
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
-  const locationMock = locationsMock[city.toLowerCase()];
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
+    return response.json(locationMock);
+  }
 
-  response.json(locationMock);
+  client
+    .geocode({
+      params: {
+        address: city,
+        key:   functions.config().google.key,     /// set google api key(places and geocode api) here via firebase functio
+                                          //// https://firebase.google.com/docs/functions/config-env
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,14 @@ const { placesRequest } = require("./places");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+
 exports.geocode = functions.https.onRequest((request, response) => {
-    geocodeRequest(request, response);
+    geocodeRequest(request, response, client);
 });
 
 exports.placesNearby = functions.https.onRequest((request, response) => {
-    placesRequest(request, response);
+    placesRequest(request, response, client);
   });
